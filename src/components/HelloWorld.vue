aec1cf1f5b98519a091ff03e83b93fdf5c0953a6
KJ <cozykoji18@gmail.com>
2020-02-14 00:50:43 +0900

    propsの型定義

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,13 +31,27 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue ,{ PropType } from 'vue';
 
 export default Vue.extend({
   name: 'HelloWorld',
   props: {
-    msg: String,
+    obj: {
+      type: Object as PropType<{name: string}>,
+      required: true
+    },
+    arr: {
+      type: Array as PropType<{task: string }[]>
+    }
   },
+  computed: {
+    myName(): string {
+      return this.obj.name
+    },
+    myFirstTask(): string {
+      return this.arr[0].task
+    }
+  }
 });
 </script>
 
